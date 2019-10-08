239810740055f1438cb9ba01997e790d5d5def21
Masanori Nakashima <haruchaco@gmail.com>
2019-10-08 13:32:43 +0900

    refactoring

--- sorter.py
+++ sorter.py
@@ -29,48 +29,53 @@ def list_files(dirpath):
         path = os.path.join(dirpath, filename)
         if 0 == os.path.getsize(path):
             print(path + " is empty file")
+            yield ("EMPTY", dirpath, filename, None)
         elif filename.startswith("._"):
             print(path + " is hidden file")
+            yield ("HIDDEN", dirpath, filename, None)
         elif os.path.isdir(path):
-            list_files(path)
+            yield from list_files(path)
         else:
             if filename.endswith(".JPG") or filename.endswith(".jpg"):
                 datetime_string = jpeg.get_datetime(path)
                 print("%s : %s" % (path, datetime_string))
+                yield ("JPG", dirpath, filename, datetime_string)
             elif filename.endswith(".CR2") or filename.endswith(".cr2"):
                 datetime_string = cr2.getCR2DateTime(path)
                 print("%s : %s" % (path, datetime_string))
+                yield ("RAW", dirpath, filename, datetime_string)
             elif filename.endswith(".MOV") or filename.endswith(".mov"):
                 datetime_string = movie.get_created_time(path)
                 print("%s : %s" % (path, datetime_string))
+                yield ("MOVIE", dirpath, filename, datetime_string)
             elif filename.endswith(".MP4") or filename.endswith(".mp4"):
                 datetime_string = movie.get_created_time(path)
                 print("%s : %s" % (path, datetime_string))
+                yield ("MOVIE", dirpath, filename, datetime_string)
             elif filename.endswith(".3GP") or filename.endswith(".3gp"):
                 datetime_string = movie.get_created_time(path)
                 print("%s : %s" % (path, datetime_string))
+                yield ("MOVIE", dirpath, filename, datetime_string)
             else:
-                print("Not an image file : %s" % path)
                 ot = os.stat(path).st_ctime
                 dt = datetime.datetime.fromtimestamp(ot)
                 datetime_string = dt.strftime("%Y:%m:%d %H:%M:%S")
                 print("%s : %s" % (path, datetime_string))
+                yield ("OTHER", dirpath, filename, datetime_string)
 
-
-
-
-def move_to_proper_dir(dirname, filename, datetime_string):
+def move_to_proper_dir(dst, ftype, dirpath, filename, datetime_string):
     """
     ファイル情報と出力先フォルダパスを受け取って適切なフォルダにファイルを移動する
     """
     [d,t] = datetime_string.split()
     [yyyy,mm,dd] = d.split(":")
-    dp = os.path.join(dst,dirname)
-    dp = os.path.join(dp,yyyy)
-    dp = os.path.join(dp,mm)
+    dp = os.path.join(dst, ftype)
+    dp = os.path.join(dp, yyyy)
+    dp = os.path.join(dp, mm)
     print(dp)
     if not os.path.exists(dp):
         os.makedirs(dp)
+    path = os.path.join(dirpath, filename)
     shutil.copy(path,dp)
 
 def main():
@@ -85,8 +90,8 @@ def main():
         print("src="+ src)
         print("dst="+ dst)
         if os.path.isdir(src) and os.path.isdir(dst):
-            list_files(args[1])
-#                move_to_proper_dir(taginfo,dst)
+            for (ftype, dirpath, filename, datetime_string) in list_files(args[1]):
+                move_to_proper_dir(dst, ftype, dirpath, filename, datetime_string)
         else:
             print(src + " or " + dst + " is not a folder")
     else:
@@ -94,7 +99,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-#    path = '/Volumes/HD-MN2017/JPEGS/2014/12/IMG_4437.CR2';
-#    datetime_string = cr2.getCR2DateTime(path)
-#    print(path + " : "+ datetime_string)
 
