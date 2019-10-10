497b309c709a00713d73f325bc3ed7ef38118241
Masanori Nakashima <haruchaco@gmail.com>
2019-10-11 01:37:44 +0900

    fix #19 movie header analysis

--- src/media/movie.py
+++ src/media/movie.py
@@ -1,9 +1,14 @@
-import subprocess, time, os, sys
+import subprocess
+import os
 
 def get_datetime(path):
-    proc = subprocess.run(["/usr/local/bin/ffprobe","-show_chapters","-hide_banner",path],
-            stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    response = proc.stdout.decode("utf8")+"\n"+(proc.stderr.decode("utf8"))
+    print("path: %s" % path)
+    if not os.path.exists(path):
+        return None
+    proc = subprocess.run(["/usr/local/bin/ffprobe","-show_chapters","-hide_banner", path],
+            stdout = subprocess.PIPE, stderr = subprocess.PIPE, encoding = "utf8", errors='ignore')
+    response = proc.stdout+"\n"+(proc.stderr)
+    print(response)
     for line in response.splitlines():
         if "creation_time" in line:
             str = line.strip().replace("creation_time","").strip().replace(": ","").strip().replace("T"," ").replace("Z","")
