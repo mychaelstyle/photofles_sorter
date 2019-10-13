d19bc59c6c7fc77a510b7ed4fff60b32133b6605
Masanori Nakashima <haruchaco@gmail.com>
2019-10-13 14:03:14 +0900

    fix #21 integrate exiftool for cr2

--- tests/media/test_cr2.py
+++ tests/media/test_cr2.py
@@ -6,3 +6,11 @@ def test_get_datetime():
     dt = cr2.get_datetime(path)
     assert dt == '2015:12:24 12:01:32'
 
+def test_is_equal():
+    path1 = './fixtures/IMG_8610.CR2'
+    path2 = './fixtures/IMG_8610.CR2'
+    assert cr2.is_equal(path1,path2)
+
+    path2 = './fixtures/IMG_5192.CR2'
+    assert not cr2.is_equal(path1,path2)
+
